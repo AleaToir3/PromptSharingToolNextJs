fe261707c8eea3acbd332a35eaf8ba876d4921fb
AleaToir3 <aleatoir3@gmail.com>
2024-10-10 10:11:03 +0100

    prompt creation logic & form submission

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,6 +2,8 @@
 import Form from '@components/Form'
 import React from 'react'
 import { useState } from 'react/'
+import { useRouter } from "next/navigation";
+
 
 const CreatePrompt = () => {
     const [submitting,setSubmitting] = useState(false)
@@ -9,10 +11,28 @@ const CreatePrompt = () => {
         prompt:'',
         tag:'',
     })
+    const router = useRouter();
+
 
     const createPrompt = async (e)=>{
-
-    }
+        e.preventDefautl();
+        setSubmitting(true);
+        try {
+            const reponse = await fetch('/api/prompt/new',{
+                method:'POST',
+                prompt: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+            if(reponse.ok) router.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+        finally {setSubmitting(false)
+        }
+}
   return (
 <Form
     type="Create"
