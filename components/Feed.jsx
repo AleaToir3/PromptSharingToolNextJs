469e9fc623bea3fe20a0b042420f8a88ca22fb64
AleaToir3 <aleatoir3@gmail.com>
2024-10-11 11:44:10 +0100

    Finalize feed  and implement fetch all  & create prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,43 @@
- 
-const Feed = () => {
+ "use client";
+
+import { useState,useEffect } from "react";
+import PromptCard from "./PromptCard";
+const PromptCardList = ({data,handleTagClick})=>{
   return (
-    <div>Feed</div>
+    <div className='mt-16 prompt_layout'>
+        {data.map((post)=>
+          <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+        )}
+    </div>
+  )
+}
+const Feed = () => {
+ const [searchText, setSearchText] = useState('');
+ const [posts,setPosts] = useState([]);
+
+ const handleSeachChange = (e)=>{
+
+ }
+ useEffect(()=>{
+  const fetchPots = async()=>{
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
+    setPosts(data)
+  }
+  fetchPots()
+ },[])
+  return (
+    <section className="feed">
+        <form className='relative w-full flex-center'>
+          <input className="search_input peer"
+          type="text"
+          placeholder="Search tag or user"
+          value={searchText}
+          onChange={handleSeachChange}
+          required />
+        </form>
+        <PromptCardList data={posts} handleTagClick = {()=>{}} />
+    </section>
   )
 }
 
