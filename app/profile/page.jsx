54fb1447810af6cfe57cd108c0c98a9257fbe89f
AleaToir3 <aleatoir3@gmail.com>
2024-10-14 02:35:17 +0100

    delete post from profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,24 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`)
   }
   const handleDelete = async(post)=>{
+    const confirmDeleteUser = confirm("Are you sur you want to delete this Prompt???");
+   
+    if(confirmDeleteUser){
+      try {
+        const reponse = await fetch(`/api/prompt/${post._id.toString()}`,{
+          method:'DELETE'            
+        });
+
+        const filteredPostWithouDeletePost = posts.filter((p)=>p._id !== post._id) 
+        setPosts(filteredPostWithouDeletePost)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    
+  
    }
+
    useEffect(()=>{
     const fetchPots = async()=>{
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
